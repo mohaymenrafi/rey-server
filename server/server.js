e712a13307fc89b8ff04065640b72332d83ced8b
mohaymenrafi <mohaymen.rafi@gmail.com>
2022-09-13 05:30:01 +0600

    404 page added

--- server/server.js
+++ server/server.js
@@ -6,6 +6,17 @@ const PORT = process.env.PORT || 5000;
 app.use("/", express.static(path.join(__dirname, "/public")));
 app.use("/", require("./routes/root"));
 
+app.all("*", (req, res) => {
+	res.status(404);
+	if (req.accepts("html")) {
+		res.sendFile(path.join(__dirname, "views", "404.html"));
+	} else if (req.accepts("json")) {
+		res.json({ message: "404 not found" });
+	} else {
+		res.type("txt").send("404 not found");
+	}
+});
+
 app.listen(PORT, () => {
 	console.log("Server is listening on port " + PORT);
 });
