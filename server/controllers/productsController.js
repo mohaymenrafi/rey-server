d751421b7d57f7bf69b3370b2cd3e606d954dd3b
mohaymenrafi <mohaymen.rafi@gmail.com>
2022-09-28 03:54:40 +0600

    basic crud for product is added, need to test

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -16,6 +16,50 @@ const getAllProducts = asyncHandler(async (req, res) => {
 // @route POST /api/products
 // @access private
 const createNewProduct = asyncHandler(async (req, res) => {
-	const { title, desc, img, price, categories, size, color, inStock } =
-		req.body;
+	const { title, desc, img, price } = req.body;
+
+	//confirm data
+	if (!title || !desc || !img || !price) {
+		return res
+			.status(400)
+			.json({ message: "Title, Desc, Img & Price must be provided" });
+	}
+	let productObject = req.body;
+	const product = await Product.create(productObject);
+	if (product) {
+		res.status(201).json({ message: `New Product ${product.title} created` });
+	} else {
+		res.status(400).json({ message: "Invalid product data received" });
+	}
 });
+
+// @desc update a product
+// @route PUT /api/products/:id
+// @access private
+const updateProduct = asyncHandler(async (req, res) => {
+	const id = req.params.id;
+	const updatedProduct = await Product.findByIdAndUpdate(
+		id,
+		{ $set: req.body },
+		{ new: true }
+	);
+	res.status(200).send(updatedProduct);
+});
+
+// @desc update a product
+// @route DELETE /api/products/:id
+// @access private
+const deleteProduct = asyncHandler(async (req, res) => {
+	const id = req.params.id;
+	const deletedProduct = await Product.findByIdAndDelete(id);
+	res
+		.status(200)
+		.json({ message: `${deletedProduct.title} has been deleted ` });
+});
+
+module.exports = {
+	getAllProducts,
+	createNewProduct,
+	updateProduct,
+	deleteProduct,
+};
