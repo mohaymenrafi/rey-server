0595d075f56b8472b535f419714e02bb795eb772
mohaymenrafi <mohaymen.rafi@gmail.com>
2022-09-27 17:04:52 +0600

    product schema added

---
+++ server/models/Product.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+	title: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	description: {
+		type: String,
+		required: true,
+	},
+	img: {
+		type: String,
+		required: true,
+	},
+	price: {
+		type: Number,
+		required: true,
+	},
+	categories: {
+		type: Array,
+	},
+	size: {
+		type: Array,
+	},
+	color: {
+		type: Array,
+	},
+	inStock: {
+		type: Boolean,
+	},
+});
+
+module.exports = mongoose.model("Product", productSchema);
