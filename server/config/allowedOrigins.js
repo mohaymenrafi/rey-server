807f5f2b4c6f7f7511ff801ab3a8d4fbc565a6e7
mohaymenrafi <mohaymen.rafi@gmail.com>
2023-01-06 15:18:00 +0600

    redux persist added and port updated

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:3000", "http://localhost:5000"];
+const allowedOrigins = [
+	"http://localhost:3000",
+	"http://localhost:3333",
+	"http://localhost:5000",
+];
 
 module.exports = allowedOrigins;
