362606a60d48ebfc1b51593fbe78efb7fcc4b1a5
mohaymenrafi <mohaymen.rafi@gmail.com>
2023-01-26 23:37:40 +0600

    cors config updated

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
 	"http://localhost:4000",
 	"http://localhost:3000",
 	"http://localhost:5000",
-	"https://tough-ray-spacesuit.cyclic.app/",
+	"https://reyfurnisher.vercel.app/",
 ];
 
 module.exports = allowedOrigins;
