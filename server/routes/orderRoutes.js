7a5dd2aeaf9b9909cfc15435ad9277338530f27b
mohaymenrafi <mohaymen.rafi@gmail.com>
2023-01-26 15:56:07 +0600

    directory updated

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const orderController = require("../controllers/orderController");
 const verifyJWT = require("../middleware/verifyJWT");
 
+//TODO: add routes for order update
+
 router.get("/:id", verifyJWT, orderController.getOrder);
 
 module.exports = router;
