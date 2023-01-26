21303d9ae35fbc3f62e724974d5d7d0cdbe51c35
mohaymenrafi <mohaymen.rafi@gmail.com>
2023-01-26 16:43:56 +0600

    cors udpated

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
 	"http://localhost:4000",
 	"http://localhost:3000",
 	"http://localhost:5000",
+	"https://tough-ray-spacesuit.cyclic.app/",
 ];
 
 module.exports = allowedOrigins;
