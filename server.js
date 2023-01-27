2e9ecfa6ef9f7cb3875726284de66d546fcfe2a3
mohaymenrafi <mohaymen.rafi@gmail.com>
2023-01-27 17:25:44 +0600

    all cors added

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
 
 connectDB();
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(logger);
 // this route is declared before express.json() middleware to get raw req.body for stripe signature verification
 app.use("/api", require("./routes/checkoutRoutes"));
