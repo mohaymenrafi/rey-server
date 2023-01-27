c3803afb84b5559c18ed2c925609101519a8692e
mohaymenrafi <mohaymen.rafi@gmail.com>
2023-01-27 15:57:41 +0600

    cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,9 @@
 const allowedOrigins = [
+	"https://rey-server.onrender.com/",
 	"https://reyfurnisher.vercel.app",
 	"http://localhost:4000",
 	"http://localhost:3000",
 	"http://localhost:5000",
-	"https://rey-server.onrender.com",
 ];
 
 module.exports = allowedOrigins;
