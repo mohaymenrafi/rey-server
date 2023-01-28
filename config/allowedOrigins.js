3b126f831ff0795b4f0f4ed2043d3579f12c6085
mohaymenrafi <mohaymen.rafi@gmail.com>
2023-01-28 15:30:29 +0600

    netlify added

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,8 @@ const allowedOrigins = [
 	"https://rey-server.onrender.com",
 	"https://reyfurnisher.vercel.app",
 	"https://rey-q3yh0k8kr-mohaymenrafi.vercel.app",
+	"https://melodious-khapse-4e21ad.netlify.app",
+	"https://reyfurnish.netlify.app/",
 	"http://localhost:4000",
 	"http://localhost:3000",
 	"http://localhost:5000",
