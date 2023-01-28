981fb6968e16a05cbb2757c2899e93cca28184e4
mohaymenrafi <mohaymen.rafi@gmail.com>
2023-01-28 12:06:39 +0600

    link added

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
 	"https://rey-server.onrender.com",
 	"https://reyfurnisher.vercel.app",
+	"https://rey-q3yh0k8kr-mohaymenrafi.vercel.app",
 	"http://localhost:4000",
 	"http://localhost:3000",
 	"http://localhost:5000",
